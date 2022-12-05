36ba1769056130ffcfb47ecd68df973c59277869
NickKros <100994354+NickKros@users.noreply.github.com>
2022-12-05 17:47:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from kivy.uix.button import Button
 
 class MyApp(App):
 	def build(self):
-		return Button(text = "Hello, world")
+		return Button(text = "Hello, world", 
+			on_press=self.func,
+			on_release=self.func1)
 
+	def func(self, instance):
+		instance.text = "Пошел нахуй"
+	def func1(self, instance):
+		instance.text = "Все равно пошел нахуй"
 
 if __name__ == "__main__":
 	MyApp().run()

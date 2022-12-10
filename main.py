b5f9fd50cd8860ec5a7f09939030b7a6d7bf7bc9
anford <mnogolistiy.mers2017@gmail.com>
2022-12-10 22:38:45 +0100

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.properties import StringProperty
 
-text = "И так, привет, не трогай меня)"
+FS = """
+firstScreen:
+	orientation: "vertical"
+
+	Label:
+		font_size: "30sp"
+		text: root.data_label
+		on_text: app.process()
+
+	TextInput:
+		id: inp
+		multiline: False
+		padding_y: (5, 5)
+		size_hint: (1, 0.5)
+
+	Button:
+		text: "classic"
+		bold: True
+		background_color: '#00FFCE'
+		size_hint: (1, 0.5)
+		on_press: root.testback()
+"""
+
+class firstScreen(BoxLayout):
+
+	data_label = StringProperty("Картошка")
+
+	def testback(self):
+		self.data_label = self.ids.inp.text
+
 
 class MyApp(App):
-	def build(self):
-		return Button(text = text, 
-			on_press=self.func,
-			on_release=self.func1)
 
-	def func(self, instance):
-		instance.text = "Пошел нахуй"
-	def func1(self, instance):
-		instance.text = "Все равно пошел нахуй"
+	def process(self):
+		text = self.root.ids.inp.text
+	
+	def build(self):
+		return Builder.load_string(FS)
 
 if __name__ == "__main__":
 	MyApp().run()

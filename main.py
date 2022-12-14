80a0e86cd624c022d003283f337f553528de7f48
anford <mnogolistiy.mers2017@gmail.com>
2022-12-14 21:31:10 +0100

    main.py

--- main.py
+++ main.py
@@ -1,48 +1,18 @@
+import os
+
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-from kivy.properties import StringProperty
+#from . import firstScreen
 
-FS = """
-firstScreen:
-	orientation: "vertical"
-
-	Label:
-		font_size: "30sp"
-		text: root.data_label
-		on_text: app.process()
-
-	TextInput:
-		id: inp
-		multiline: False
-		padding_y: (5, 5)
-		size_hint: (1, 0.5)
-
-	Button:
-		text: "classic"
-		bold: True
-		background_color: '#00FFCE'
-		size_hint: (1, 0.5)
-		on_press: root.testback()
-"""
-
-class firstScreen(BoxLayout):
-
-	data_label = StringProperty("Картошка")
-
-	def testback(self):
-		self.data_label = self.ids.inp.text
-
+#class firstScreen(BoxLayout):
+#		pass
 
 class MyApp(App):
 
-	def process(self):
-		text = self.root.ids.inp.text
-	
 	def build(self):
-		return Builder.load_string(FS)
+		main_widget = Builder.load_file("firstScreen.kv")
+		return main_widget
 
 if __name__ == "__main__":
 	MyApp().run()

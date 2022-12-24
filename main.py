a9be59f82cde8af75bda404766f3e999cf066d7c
NickKros <100994354+NickKros@users.noreply.github.com>
2022-12-24 13:35:57 +0100

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-import os
-
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
-from config import KIVYDIR
-#from . import firstScreen
+from kivy.properties import StringProperty
 
-#class firstScreen(BoxLayout):
-#		pass
+KV = """
+MyBL:
+	orientation: "vertical"
+
+	Label:
+		font_size: "30sp"
+		text: root.label_text
+
+	TextInput:
+		multiline: False
+		size_hint: (1, 0.5)
+
+	Button:
+		text: "classic"
+		bold: True
+		background_color: '#00FFCE'
+		size_hint: (1, 0.5)
+"""
+
+class MyBL(BoxLayout):
+	
+	label_text = StringProperty("Что-то")
+
 
 class MyApp(App):
 
 	def build(self):
-		main_widget = Builder.load_file(os.path.join(KIVYDIR, "firstScreen.kv"))
-		return main_widget
+		return Builder.load_string(KV)
+		'''
+		main_widget = Builder.load_string(KV)
+		return main_widget'''
 
-#if __name__ == "__main__":
-MyApp().run()
+if __name__ == "__main__":
+	MyApp().run()
